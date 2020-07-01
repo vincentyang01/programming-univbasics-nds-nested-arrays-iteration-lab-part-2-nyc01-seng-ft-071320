43d24120f0232aab16002c3b307767f8748a4578
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-01 20:42:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,8 +3,17 @@ def find_min_in_nested_arrays(src)
   # Produce a new Array that contains the smallest number of each of the nested arrays
   arrayOfMins = []
   row_index = 0;
+  min = 1000
   while row_index < src.count do
     rowElements = 0
-    while element_index < src[row_index].count do
-      
+    while rowElements < src[row_index].count do
+      if min > src[row_index][rowElements]
+        min = src[row_index][rowElements]
+      end
+      rowElements+=1;
+    end
+    arrayOfMins.push(min)
+    row_index+=1;
+  end
+  arrayOfMins 
 end
